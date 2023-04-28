3bc39f4eba8b7fc7cc6e2c9ed6862ffe5462bf46
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2023-04-28 12:55:36 +0300

    Display post content

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import fs from 'fs'
+import Markdown from 'markdown-to-jsx'
 
-const PostPage = () => {
-  return <div>This is a post!</div>
+const getPostContent = (slug: string) => {
+  const folder = 'posts/'
+  const file = `${folder}${slug}.md`
+  const content = fs.readFileSync(file, 'utf-8')
+  return content
+}
+
+const PostPage = (props: any) => {
+  const slug = props.params.slug
+  const content = getPostContent(slug)
+  return (
+    <div>
+      <h1>This is a post: {slug}</h1>
+      <Markdown>{content}</Markdown>
+    </div>
+  )
 }
 
 export default PostPage
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className='p-8'>
+      <Link href='/'>
+        <h1>Ozan's Blog</h1>
+      </Link>
+    </div>
+  )
 }
 
 export default Header
