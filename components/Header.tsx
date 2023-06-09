55daf755392c1c29db73ef429135011faa67e7cb
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2023-06-09 22:30:03 +0300

    Add header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,40 @@
-import React from 'react'
-
-export const Header = () => {
-  return <div>Header</div>
+const Header = () => {
+  return (
+    <header
+      style={{ boxShadow: '4px 6px 13px rgba(215, 215, 215, 0.25)' }}
+      className='bg-light-grey-1  px-[95px]'>
+      <div className='text-white py-4 max-w-[1440px] mx-auto flex flex-col md:flex-row justify-between items-center'>
+        <div className='flex items-baseline justify-center mb-4 md:mb-0'>
+          <span className='text-[#001858] font-semibold text-4xl'>Ozan</span>
+          <span className='text-blue text-lg font-bold'>.Blog</span>
+        </div>
+        <nav>
+          <ul className='flex flex-col space-y-4 md:flex-row md:gap-9 md:space-y-0 md:space-x-4'>
+            <li>
+              <a className='subheading-2 text-black' href='#'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a className='subheading-2 text-black' href='#'>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className='subheading-2 text-black' href='#'>
+                Blog
+              </a>
+            </li>
+            <li>
+              <a className='subheading-2 text-black' href='#'>
+                About Me
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      white: '#FFFFFE',
+      'light-grey-1': '#EFF0F3',
+      'light-grey-2': '#E4E5E9',
+      grey: '#C0C0C0',
+      'dark-grey': '#9A9494',
+      black: '#2B2C34',
+      blue: '#6246EA'
+    }
   },
-  plugins: [require("@tailwindcss/typography")],
-};
+  plugins: [require('@tailwindcss/typography')]
+}
