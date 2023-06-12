23013c03d186231415c40ca038aa14a8093d77e0
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2023-06-12 14:42:43 +0300

    Add menu icon for mobile version

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,32 @@
 'use client'
+import cx from 'classnames'
 import Link from 'next/link'
-import { RiFileDownloadLine } from 'react-icons/ri'
+import { useState } from 'react'
+import { RiCloseLine, RiFileDownloadLine, RiMenuLine } from 'react-icons/ri'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <header
       style={{ boxShadow: '4px 6px 13px rgba(215, 215, 215, 0.25)' }}
       className='bg-light-grey-1 sticky top-0 z-50'>
       <div className='text-white py-4 max-w-[1250px] mx-auto flex flex-col px-4 justify-between items-center md:flex-row xl:px-0'>
-        <div className='flex items-baseline justify-center mb-4 md:mb-0'>
+        <div className='items-baseline justify-center mb-4 hidden md:flex md:mb-0 '>
           <span className='text-[#001858] font-semibold text-4xl'>Ozan</span>
           <span className='text-blue text-lg font-bold'>.Blog</span>
         </div>
-        <nav>
+        <div className='ml-auto md:hidden'>
+          <button className='text-blue' onClick={toggleMenu}>
+            {isMenuOpen ? <RiCloseLine size={24} /> : <RiMenuLine size={24} />}
+          </button>
+        </div>
+        <nav
+          className={cx('md:flex', { flex: isMenuOpen, hidden: !isMenuOpen })}>
           <ul className='flex flex-col space-y-4 md:flex-row items-center md:gap-9 md:space-y-0 md:space-x-4'>
             <li>
               <Link className='subheading-2 text-black' href='/'>
