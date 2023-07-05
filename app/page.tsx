7215bd320bb4a2052e7fc739cb76a4a9079d3ddf
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2023-07-05 14:04:12 +0300

    Fix article bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,6 @@
-import fs from 'fs'
-import matter from 'gray-matter'
 import { ArticleSection } from '../components/ArticleSection'
 import { Hero } from '../components/Hero'
-import { PostMetadata } from '../components/types'
-
-const getPostMetadata = (): PostMetadata[] => {
-  const folder = 'posts/'
-  const files = fs.readdirSync(folder)
-  const markdownArticles = files.filter((file) => file.endsWith('.md'))
-
-  // Get gray-matter data from each file.
-  const articles = markdownArticles.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, 'utf8')
-    const matterResult = matter(fileContents)
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      img: matterResult.data.img,
-      category: matterResult.data.category,
-      slug: fileName.replace('.md', '')
-    }
-  })
-
-  return articles
-}
+import { getPostMetadata } from '../components/utils'
 
 const HomePage = () => {
   const articles = getPostMetadata()
---
+++ components/utils.ts
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import { PostMetadata } from './types'
+
+export const getPostMetadata = (): PostMetadata[] => {
+  const folder = 'posts/'
+  const files = fs.readdirSync(folder)
+  const markdownArticles = files.filter((file) => file.endsWith('.md'))
+
+  // Get gray-matter data from each file.
+  const articles = markdownArticles.map((fileName) => {
+    const fileContents = fs.readFileSync(`posts/${fileName}`, 'utf8')
+    const matterResult = matter(fileContents)
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      img: matterResult.data.img,
+      category: matterResult.data.category,
+      slug: fileName.replace('.md', '')
+    }
+  })
+
+  return articles
+}
