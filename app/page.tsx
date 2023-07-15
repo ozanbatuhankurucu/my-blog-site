2457c20122e43c2b9d5b179817dceb7e33497c98
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2023-07-16 00:34:23 +0300

    Add seo for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import { NextSeo, ArticleJsonLd } from 'next-seo'
 import { ArticleSection } from '../components/ArticleSection'
 import { Hero } from '../components/Hero'
 import { getPostMetadata } from '../components/utils'
@@ -7,12 +8,27 @@ const HomePage = () => {
   const articles = getPostMetadata()
   const groupedArticlesByCategory = _.groupBy(articles, 'category')
 
+  // SEO information
+  const SEO = {
+    title: 'Ozan Batuhan Kurucu - Blog',
+    description: 'Welcome to the blog of Ozan Batuhan Kurucu',
+    openGraph: {
+      title: 'Ozan Batuhan Kurucu - Blog',
+      description: 'Welcome to the blog of Ozan Batuhan Kurucu',
+      type: 'website',
+      url: 'https://www.ozanbatuhankurucu.com',
+      site_name: 'Ozan Batuhan Kurucu Blog'
+    }
+  }
+
   return (
     <>
+      <NextSeo {...SEO} />
       <Hero />
       <div className='max-w-[1250px] mx-auto px-4 pb-56'>
         {_.map(groupedArticlesByCategory, (articles, key) => (
           <ArticleSection
+            key={key}
             category={key}
             sectionTitle={key}
             articles={articles}
