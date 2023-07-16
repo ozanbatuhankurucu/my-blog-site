f7d6767a30273dd71fb3976c809a9a8f61a2deca
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2023-07-16 13:09:18 +0300

    test seo

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,28 @@
 export default function Head() {
   return (
     <>
-      <title>Ozan's Blog</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <title>Ozan Batuhan Kurucu - Blog</title>
+      <meta
+        name='description'
+        content='Welcome to the blog of Ozan Batuhan Kurucu'
+      />
+      <meta property='og:title' content='Ozan Batuhan Kurucu - Blog' />
+      <meta
+        property='og:description'
+        content='Welcome to the blog of Ozan Batuhan Kurucu'
+      />
+      <meta property='og:url' content='https://www.ozanbatuhankurucu.com' />
+      <meta property='og:type' content='website' />
+      <meta property='og:site_name' content='Ozan Batuhan Kurucu Blog' />
+      <meta
+        property='og:image'
+        content='https://www.ozanbatuhankurucu.com/images/mainpage.png'
+      />
+      <meta property='og:image:width' content='1200' />
+      <meta property='og:image:height' content='630' />
+      <meta property='og:image:alt' content='Main Page Image' />
+
       <link rel='icon' type='image/png' sizes='96x96' href='/favicon96.png' />
     </>
   )
