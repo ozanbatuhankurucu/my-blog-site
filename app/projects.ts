48815c925f03bbac72754a5430cd4a7f2fe3af57
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2023-09-08 12:31:01 +0300

    Update projects

--- app/projects.ts
+++ app/projects.ts
@@ -76,6 +76,7 @@ export const PROJECTS: Project[] = [
     updatedAt: '2022-05-29',
     url: 'https://master.d3c1rfe7yqyxzj.amplifyapp.com/projects',
     features: [
+      'Animated Navigation',
       'Random Choice Picker',
       'FAQ Collapse',
       'Event KeyCodes',
