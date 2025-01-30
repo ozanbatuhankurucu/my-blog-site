0113362d30e2c53a3d9ff525f96b3cf6e2c50317
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2025-01-30 19:13:21 +0300

    Add AI category into Category Order

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,38 @@
-import _ from 'lodash'
-import { Metadata } from 'next'
-import { useMemo } from 'react'
-import { ArticleSection } from '../components/ArticleSection'
-import { Hero } from '../components/Hero'
-import { CategoryType, PostMetadata } from '../components/types'
-import { getPostMetadata } from '../components/utils'
+import _ from "lodash"
+import { Metadata } from "next"
+import { useMemo } from "react"
+import { ArticleSection } from "../components/ArticleSection"
+import { Hero } from "../components/Hero"
+import { CategoryType, PostMetadata } from "../components/types"
+import { getPostMetadata } from "../components/utils"
 
 const CATEGORY_ORDER: CategoryType[] = [
-  'Featured Article',
-  'Self-improvement',
-  'NextJS',
-  'TypeScript',
-  'JavaScript',
-  'HTML',
-  'CSS',
-	'TailwindCSS',
-  'ReactJS'
+  "Featured Article",
+  "Artificial Intelligence",
+  "Self-improvement",
+  "NextJS",
+  "TypeScript",
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "TailwindCSS",
+  "ReactJS"
 ]
 
 export const metadata: Metadata = {
-  title: 'Ozan Batuhan Kurucu - Blog',
-  description: 'Welcome to the blog of Ozan Batuhan Kurucu',
+  title: "Ozan Batuhan Kurucu - Blog",
+  description: "Welcome to the blog of Ozan Batuhan Kurucu",
   openGraph: {
-    title: 'Ozan Batuhan Kurucu - Blog',
-    description: 'Welcome to the blog of Ozan Batuhan Kurucu',
-    url: 'https://www.ozanbatuhankurucu.com',
-    type: 'website',
-    siteName: 'Ozan Batuhan Kurucu Blog',
+    title: "Ozan Batuhan Kurucu - Blog",
+    description: "Welcome to the blog of Ozan Batuhan Kurucu",
+    url: "https://www.ozanbatuhankurucu.com",
+    type: "website",
+    siteName: "Ozan Batuhan Kurucu Blog",
     images: {
-      url: 'https://www.ozanbatuhankurucu.com/images/mainpage.png',
+      url: "https://www.ozanbatuhankurucu.com/images/mainpage.png",
       width: 1200, // Specify the width of the image in pixels
       height: 630, // Specify the height of the image in pixels
-      alt: 'Main Page Image' // Optional alt text for the image
+      alt: "Main Page Image" // Optional alt text for the image
     }
   }
 }
@@ -39,7 +40,7 @@ export const metadata: Metadata = {
 const HomePage = () => {
   const sortedGroupedArticlesByCategory = useMemo(() => {
     const articles = getPostMetadata()
-    const groupedArticlesByCategory = _.groupBy(articles, 'category')
+    const groupedArticlesByCategory = _.groupBy(articles, "category")
     const sortedObject: Partial<Record<CategoryType, PostMetadata[]>> = {}
     CATEGORY_ORDER.forEach((key) => {
       if (groupedArticlesByCategory.hasOwnProperty(key)) {
@@ -54,12 +55,7 @@ const HomePage = () => {
       <Hero />
       <div className='max-w-[1250px] mx-auto px-4 pb-56'>
         {_.map(sortedGroupedArticlesByCategory, (articles, key) => (
-          <ArticleSection
-            key={key}
-            category={key}
-            sectionTitle={key}
-            articles={articles}
-          />
+          <ArticleSection key={key} category={key} sectionTitle={key} articles={articles} />
         ))}
       </div>
     </>
